3207909e2d47f99c06cd731f2be8fc137c7c96a1
spandanb <spandan@savinetwork.ca>
2014-07-01 18:37:14 +0000

    added sahara dashboard config file changes

--- openstack_dashboard/settings.py
+++ openstack_dashboard/settings.py
@@ -56,7 +56,7 @@ STATIC_URL = '/static/'
 ROOT_URLCONF = 'openstack_dashboard.urls'
 
 HORIZON_CONFIG = {
-    'dashboards': ('project', 'admin', 'settings', 'router',),
+    'dashboards': ('project', 'admin', 'settings', 'router','sahara',),
     'default_dashboard': 'project',
     'user_home': 'openstack_dashboard.views.get_user_home',
     'ajax_queue_limit': 10,
@@ -156,6 +156,7 @@ INSTALLED_APPS = [
     'compressor',
     'horizon',
     'openstack_auth',
+    'saharadashboard',
 ]
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
