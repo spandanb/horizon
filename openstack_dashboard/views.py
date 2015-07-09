ca47783f22d03de17cdcda3df0d829d1adc1263f
spandanb <spandan@savinetwork.ca>
2015-07-09 19:56:03 +0000

    Changed default page from management to project page

--- openstack_dashboard/views.py
+++ openstack_dashboard/views.py
@@ -25,8 +25,7 @@ from openstack_auth import views
 def get_user_home(user):
     if user.is_superuser:
         return horizon.get_dashboard('admin').get_absolute_url()
-    #return horizon.get_dashboard('project').get_absolute_url()
-    return horizon.get_dashboard('management').get_absolute_url()
+    return horizon.get_dashboard('project').get_absolute_url()
 
 
 @vary.vary_on_cookie
